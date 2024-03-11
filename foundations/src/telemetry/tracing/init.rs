b2887514fdf6e988ed627ab73802235ac892821b
inikulin <inikulin@cloudflare.com>
2024-03-11 12:32:52 +0000

    Make tracer exporters configurable

--- foundations/src/telemetry/tracing/init.rs
+++ foundations/src/telemetry/tracing/init.rs
@@ -1,22 +1,14 @@
 use super::internal::{FinishedSpan, SharedSpan, Tracer};
+use super::jaeger_thrift_udp_exporter;
 use crate::telemetry::scope::ScopeStack;
-use crate::telemetry::settings::TracingSettings;
+use crate::telemetry::settings::{TracesExporter, TracingSettings};
 use crate::{BootstrapResult, ServiceInfo};
-use anyhow::bail;
 use crossbeam_channel::Receiver;
 use once_cell::sync::{Lazy, OnceCell};
-
-use rustracing::tag::Tag;
-use rustracing_jaeger::reporter::JaegerCompactReporter;
-use std::net::Ipv6Addr;
-use std::thread;
-use std::time::Duration;
 
 #[cfg(feature = "testing")]
 use std::borrow::Cow;
 
-#[cfg(feature = "logging")]
-use crate::telemetry::log;
 use crate::telemetry::tracing::rate_limit::RateLimitingProbabilisticSampler;
 
 static HARNESS: OnceCell<TracingHarness> = OnceCell::new();
@@ -83,7 +75,11 @@ pub(crate) fn init(service_info: &ServiceInfo, settings: &TracingSettings) -> Bo
     if settings.enabled {
         let (tracer, span_rx) = create_tracer_and_span_rx(settings, false)?;
 
-        start_reporter(service_info, settings, span_rx)?;
+        match &settings.exporter {
+            TracesExporter::JaegerThriftUdp(exporter_settings) => {
+                jaeger_thrift_udp_exporter::start(service_info, exporter_settings, span_rx)?
+            }
+        }
 
         let harness = TracingHarness {
             tracer,
@@ -95,54 +91,6 @@ pub(crate) fn init(service_info: &ServiceInfo, settings: &TracingSettings) -> Bo
 
         let _ = HARNESS.set(harness);
     }
-
-    Ok(())
-}
-
-fn start_reporter(
-    service_info: &ServiceInfo,
-    settings: &TracingSettings,
-    span_rx: Receiver<FinishedSpan>,
-) -> BootstrapResult<()> {
-    const REPORTER_COOLDOWN_PERIOD: Duration = Duration::from_secs(2);
-
-    let mut reporter = JaegerCompactReporter::new(service_info.name)?;
-
-    reporter.add_service_tag(Tag::new("app.version", service_info.version));
-    reporter.set_agent_addr(settings.jaeger_tracing_server_addr.into());
-
-    match settings.jaeger_reporter_bind_addr {
-        Some(addr) => {
-            // the reporter socket will attempt to send traffic to the
-            // agent address, so they have to use the same address family
-            if settings.jaeger_tracing_server_addr.is_ipv6() == addr.is_ipv6() {
-                reporter.set_reporter_addr(addr.into())?;
-            } else {
-                bail!("`jaeger_tracing_server_addr` and `jaeger_reporter_bind_addr` must have the same address family");
-            }
-        }
-        None => {
-            // caused by https://github.com/sile/rustracing_jaeger/blob/bc7d03f2f6ac6bc0269542089c8907279706ecb7/src/reporter.rs#L34,
-            // we need to also set the reporter to an ipv6 when agent is ipv6
-            if settings.jaeger_tracing_server_addr.is_ipv6() {
-                reporter.set_reporter_addr((Ipv6Addr::LOCALHOST, 0).into())?;
-            }
-        }
-    };
-
-    thread::spawn(move || {
-        while let Ok(span) = span_rx.recv() {
-            if let Err(e) = reporter.report(&[span][..]) {
-                #[cfg(feature = "logging")]
-                log::warn!("failed to send a tracing span to the agent"; "error" => %e);
-
-                #[cfg(not(feature = "logging"))]
-                drop(e);
-
-                thread::sleep(REPORTER_COOLDOWN_PERIOD);
-            }
-        }
-    });
 
     Ok(())
 }
