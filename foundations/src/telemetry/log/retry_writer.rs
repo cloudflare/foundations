68538d9203bd317a1bb8d8740d6b36362062ab25
Andrew Berglund <aberglund@cloudflare.com>
2025-01-24 10:48:39 -0600

    Cleanup unit tests Remove nanoid Remove fifo check, it doesn't fit our use case

--- foundations/src/telemetry/log/retry_writer.rs
+++ foundations/src/telemetry/log/retry_writer.rs
@@ -1,18 +1,17 @@
 use std::fs::{File, OpenOptions};
+use std::io;
 use std::io::Write;
-use std::os::unix::fs::FileTypeExt;
 use std::path::PathBuf;
-use std::{fs, io};
 
 /// A writer which retries if an [`io::ErrorKind::BrokenPipe`] is returned.
+///
+/// Gives up and return an error after the max number of attempts or if any other errors are encountered.
 ///
 /// Intended for use when writing to a pipe (fifo) with a reader which may randomly close and then
 /// reopen read side.
 ///
 /// *WARNING*: This may cause duplication of data as it's not possible to know what bytes
 /// where actually processed by the read side.
-///
-/// Gives up and return an error after the max number of attempts or if any other errors are encountered.
 pub(crate) struct RetryPipeWriter {
     path: PathBuf,
     pipe_file: Box<File>,
@@ -21,15 +20,15 @@ pub(crate) struct RetryPipeWriter {
 
 impl RetryPipeWriter {
     pub fn new(path: PathBuf) -> io::Result<Self> {
-        let meta = fs::metadata(&path)?;
-        if !meta.file_type().is_fifo() {
-            return Err(io::Error::new(io::ErrorKind::InvalidInput, "Not a fifo"));
-        }
         let file = Self::open_file(&path)?;
         Ok(Self {
             path,
             pipe_file: file,
-            max_attempts: 1,
+            // This number was selected by casually observing unit test failures.
+            // It's assumed that this simple approach will cover most cases.
+            // Further observation may show the need to either make this configurable
+            // or use an entirely differeing retry strategy.
+            max_attempts: 10,
         })
     }
 
@@ -46,9 +45,6 @@ impl RetryPipeWriter {
 impl Write for RetryPipeWriter {
     fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
         let mut attempts = 0;
-        // TODO: Need to understand if this is an appropriate retry strategy
-        // we may actually want this to be unlimited with a backoff. But if we assume
-        // that an open blocks until both sides are open then a backoff doesn't do anything.
         while attempts <= self.max_attempts {
             let result = self.pipe_file.write(buf);
             match result {
@@ -81,22 +77,24 @@ impl Write for RetryPipeWriter {
 #[cfg(test)]
 mod tests {
     use crate::telemetry::log::retry_writer::RetryPipeWriter;
-    use nanoid::nanoid;
     use nix::sys::stat;
     use nix::unistd;
-    use std::fs::OpenOptions;
-    use std::io::{BufWriter, Read, Write};
+    use std::fs::{self, OpenOptions};
+    use std::io::{Read, Write};
     use std::thread;
-    use tempfile::tempdir;
+    use tempfile::NamedTempFile;
 
     #[test]
     fn test_retry_file() {
-        let tmp_dir = tempdir().unwrap();
-        let fifo_path = tmp_dir.path().join(format!("{}.pipe", nanoid!()));
+        let tmp_path = NamedTempFile::new().unwrap().into_temp_path();
+        let fifo_path = tmp_path.to_path_buf();
+        tmp_path.close().unwrap();
+
         unistd::mkfifo(&fifo_path, stat::Mode::S_IRWXU).unwrap();
 
         let path_copy = fifo_path.clone();
-        // reader should get 10 bytes
+        // This reader thread should read 1 byte 10 times,
+        // the pipe is closed and re-opened after each read.
         let handler = thread::spawn(move || {
             let mut buffer = [0; 1];
             let mut i = 0;
@@ -108,21 +106,20 @@ mod tests {
                         .unwrap();
                     let _ = reader.read(&mut buffer[..]).unwrap();
                 }
-                i += 1;
+                i += 1
             }
         });
 
-        let mut retrying_file =
-            BufWriter::with_capacity(2, RetryPipeWriter::new(fifo_path.clone()).unwrap());
+        let mut retrying_file = RetryPipeWriter::new(fifo_path.clone()).unwrap();
         let mut i = 0;
-        // writer should send 10 bytes
+        // Send 1 bytes 10 times, unwrap the result to surface any errors not handled by the retry.
         while i < 10 {
             let _ = retrying_file.write(format!("{i}").as_bytes()).unwrap();
-            retrying_file.flush().unwrap();
             i += 1;
         }
-        // and then the reader thread should exit and complete the test
-        // if not then this test hangs and the runner can complain about it.
+        // If all goes to plan, the reader thread should see all 10 bytes, exit, and complete the test.
+        // If not then this test hangs and the runner can complain about it.
         handler.join().unwrap();
+        fs::remove_file(fifo_path).unwrap();
     }
 }
