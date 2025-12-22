f36315883733fd136bd49e4dff86e09029d327fd
Fisher Darling <fisher@cloudflare.com>
2025-12-22 12:14:16 +0100

    attempt to filter sentry events before incrementing the metric and touch up docs.

--- foundations/src/sentry/hook.rs
+++ foundations/src/sentry/hook.rs
@@ -61,9 +61,10 @@ impl SentryHookBuilder<HasRegistry<DefaultRegistry>> {
 impl<R: SentryMetricsRegistry + 'static> SentryHookBuilder<HasRegistry<R>> {
     /// Install the sentry hook on the provided client options.
     ///
-    /// This installs a `before_send` hook that increments `sentry_events_total`.
-    /// If a previous `before_send` hook exists, it will be called after incrementing
-    /// the metric.
+    /// This installs a `before_send` hook that increments
+    /// `sentry_events_total`. If a previous `before_send` hook exists, it will
+    /// be called before incrementing the metric. Only unfiltered events are
+    /// counted.
     pub fn install(self, options: &mut sentry_core::ClientOptions) {
         use std::sync::Arc;
 
@@ -71,13 +72,17 @@ impl<R: SentryMetricsRegistry + 'static> SentryHookBuilder<HasRegistry<R>> {
         let previous = options.before_send.take();
 
         options.before_send = Some(Arc::new(move |event| {
-            registry.inc_sentry_events_total(event.level, 1);
-
-            if let Some(ref prev) = previous {
+            let event = if let Some(prev) = &previous {
                 prev(event)
             } else {
                 Some(event)
+            };
+
+            if let Some(event) = &event {
+                registry.inc_sentry_events_total(event.level, 1);
             }
+
+            event
         }));
     }
 }
--- foundations/src/sentry/mod.rs
+++ foundations/src/sentry/mod.rs
@@ -1,12 +1,18 @@
 #![allow(clippy::needless_doctest_main)]
 //! Sentry hook for tracking sentry events with metrics.
 //!
-//! This module provides a sentry hook that increments the `sentry_events_total`
-//! metric for each sentry event. If a previous `before_send` hook exists, it
-//! will be executed after the installed foundations hook.
+//! This module provides a sentry hook that increments the
+//! `sentry_events_total{level=<...>}` metric for each sentry event. If a
+//! previous `before_send` hook exists, it will be executed before the installed
+//! foundations hook. Only unfiltered events are counted.
 //!
 //! This does not require the `metrics` feature to be enabled. If foundations
 //! users do not enable it, then a [`SentryMetricsRegistry`] must be provided.
+//!
+//! **note**: a clone of a client's [`sentry_core::ClientOptions`] will have the
+//! hook installed. This means "child" sentry clients will still increment the
+//! `sentry_events_total` metric. A reinstall is only required if the
+//! [`sentry_core::ClientOptions::before_send`] field is overwritten.
 //!
 //! # Usage
 //!
