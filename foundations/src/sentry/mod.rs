85d0c8cf5ecabe7452f43bd431f33e93964537b8
Leo Blöcher <lblocher@cloudflare.com>
2026-03-24 17:58:57 +0000

    Add configurable rate limiting for sentry events

--- foundations/src/sentry/mod.rs
+++ foundations/src/sentry/mod.rs
@@ -1,15 +1,24 @@
 #![allow(clippy::needless_doctest_main)]
-//! Sentry hook for tracking sentry events with metrics.
+//! Sentry hook for tracking sentry events with metrics and rate-limiting them.
 //!
 //! This module provides a sentry hook that increments the
 //! `sentry_events_total{level=<...>}` metric for each sentry event. If a
-//! previous `before_send` hook exists, it will be executed before the installed
-//! foundations hook. Only unfiltered events are counted.
+//! previous `before_send` hook exists, it will be executed after rate limiting
+//! and before the metric is incremented. Only unfiltered events are counted.
+//!
+//! For rate-limiting, we group events by fingerprint. Each group has a separate
+//! rate limiter. The fingerprint of a sentry event is the first out of the following
+//! attributes that is present and not defaulted:
+//!
+//! 1. Explicit `event.fingerprint`
+//! 2. Event message
+//! 3. First exception value, or exception type if no value is set
+//! 4. Fallback: event level name (e.g., `error`)
 //!
 //! **note**: a clone of a client's [`sentry_core::ClientOptions`] will have the
-//! hook installed. This means "child" sentry clients will still increment the
-//! `sentry_events_total` metric. A reinstall is only required if the
-//! [`sentry_core::ClientOptions::before_send`] field is overwritten.
+//! hook installed. This means "child" sentry clients will inherit the hook. A
+//! reinstall is only required if the [`sentry_core::ClientOptions::before_send`]
+//! field is overwritten.
 //!
 //! # Usage
 //!
@@ -18,7 +27,8 @@
 //! ```rust
 //! fn main() {
 //!     let mut client_opts = sentry_core::ClientOptions::default();
-//!     foundations::sentry::install_hook(&mut client_opts);
+//!     let sentry_settings = foundations::sentry::SentrySettings::default();
+//!     foundations::sentry::install_hook_with_settings(&mut client_opts, &sentry_settings);
 //!     // sentry::init(client_opts);
 //! }
 //! ```
@@ -26,6 +36,10 @@
 pub mod metrics;
 
 mod hook;
+mod settings;
 
+#[allow(deprecated)]
 pub use self::hook::install_hook;
+pub use self::hook::install_hook_with_settings;
+pub use self::settings::SentrySettings;
 pub use sentry_core::Level;
